3f5e2341b4eb90077c8dadb08c39a3776ba17d29
Byron Myo <108325816+Byron-Myo@users.noreply.github.com>
2023-03-16 14:17:46 +0630

    Update Test

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ window.addEventListener("mousemove", function (event) {
 
 ctx.fillStyle = "yellow";
 ctx.font = "2vw ZawgyiOne";
-ctx.fillText("သီဟ", 60, 60);
+ctx.fillText("T", 60, 60);
 const textCoordinates = ctx.getImageData(0, 0, 500, 500);
 
 class Particle {
